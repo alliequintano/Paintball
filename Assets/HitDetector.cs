2a4a62673a6b746083ea9fd083cb67639123ce46
nick maxwell <nmaxwell@thoughtworks.com>
2016-09-07 15:57:24 -0400

    trees change color too

--- Assets/HitDetector.cs
+++ Assets/HitDetector.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class HitDetector : MonoBehaviour {
+
+    public GameObject paintballSplatPrefab;
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.CompareTag("paintball"))
@@ -10,7 +13,9 @@ public class HitDetector : MonoBehaviour {
             Material material = renderer.material;
             material.color = GameManager.paintColor;
             GameManager.incrementScore();
+            GameObject paintballSplat = (GameObject)Instantiate(paintballSplatPrefab, col.transform.position, col.transform.rotation);
             Destroy(col.gameObject);
+            Destroy(paintballSplat, 5.0f);
         }
     }
 }
