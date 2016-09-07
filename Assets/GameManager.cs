9afb52f62ef8f8b13a0935b98c790f779d58b293
nick maxwell <nmaxwell@thoughtworks.com>
2016-09-07 14:00:43 -0400

    paintball hitting cube

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    static public int gameScore;
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            Init();
+            return instance;
+        }
+    }
+
+    static void Init()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+        }
+    }
+
+    void Awake () {
+        Init();
+	}
+
+    public static void incrementScore()
+    {
+        gameScore += 5;
+        Debug.Log(gameScore);
+    }
+}
---
+++ Assets/HitDetector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitDetector : MonoBehaviour {
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("paintball"))
+        {
+            GameManager.incrementScore();
+            Destroy(col.gameObject);
+        }
+    }
+}
